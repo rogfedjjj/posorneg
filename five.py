b32cc38e5526c2b3546c9c3eabb82033c9233481
rogfedjjj <42170257+rogfedjjj@users.noreply.github.com>
2019-06-06 12:25:49 +0530

    Create five.py

---
+++ five.py
@@ -0,0 +1,9 @@
+a= input("Enter any number: ")
+b= input("Enter any number: ")
+c= input("Enter any number: ")
+if (a>b) and (a>c):
+    print(a)
+elif (b>c) and (b>a):
+    print(b)
+else:
+    print(c)
